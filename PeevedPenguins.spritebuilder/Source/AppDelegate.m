9d06a630676fbae7f1cc3652b56425b1e4f78233
EtiennevanderWalt <etienne.van.der.walt@gmail.com>
2014-10-26 18:32:55 +1100

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/AppDelegate.m
+++ PeevedPenguins.spritebuilder/Source/AppDelegate.m
@@ -54,7 +54,7 @@
     
     [self setupCocos2dWithOptions:cocos2dSetup];
     
-    [CCBReader load:@"Penguin"];
+    [CCBReader load:@"Pengiun"];
     [CCBReader load:@"Seal"];
     
     return YES;
