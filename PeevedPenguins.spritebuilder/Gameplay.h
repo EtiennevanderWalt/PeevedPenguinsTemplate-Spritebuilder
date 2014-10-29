bdf405eddad20eefff98694741d23a0ee99ee5fa
EtiennevanderWalt <etienne.van.der.walt@gmail.com>
2014-10-29 18:59:41 +1100

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  CCNode+Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Etienne van der Walt on 29/10/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay: CCNode
+
+@end
---
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -0,0 +1,13 @@
+//
+//  CCNode+Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Etienne van der Walt on 29/10/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay
+
+@end
